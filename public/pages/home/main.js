229cf990a681f284359e8d229759a18103e4e278
Jucilene Barros Silva <barrossilvajucilene@gmail.com>
2020-05-30 11:28:19 -0300

    instalação de firebase e ajuste dos inputs

--- public/pages/home/main.js
+++ public/pages/home/main.js
@@ -1,21 +1,21 @@
 // Aqui serão criados os eventos de Manipulação de DOM e templates
-import { greeting } from './data.js';
+import { greeting } from "./data.js";
 
 export const home = () => {
-  const container = document.createElement('div');
+  const container = document.createElement("div");
 
   container.innerHTML = `
     <form>
-      <input id='email' type='text'>
-      <input id=' 
-      <button id='greeting-btn'>Log in</button>
+      <input id="email" type="text">
+      <input id="password" type= "text" 
+      <button id="login-btn">Log in</button>
     </form>
-    <div id='greeting-message'></div>
+    <div id="greeting-message"></div>
   `;
 
-  const name = container.querySelector('#email');
-  const greetingBtn = container.querySelector('#greeting-btn');
-  const greetingMessage = container.querySelector('#greeting-message');
+  const name = container.querySelector("#email");
+  const logInBtn = container.querySelector("#login-btn");
+  const greetingMessage = container.querySelector("#greeting-message");
 
   greetingBtn.addEventListener('click', (event) => {
     event.preventDefault();
