46962cb7982a81ab775a767193c52a6213e3cc80
Milena Ferraz <milenaferraz@gmail.com>
2020-05-31 10:48:05 -0300

    //wip

--- public/index.js
+++ public/index.js
@@ -1,11 +1,26 @@
 // Este é o ponto de entrada de sua aplicação
 import { home } from './pages/home/main.js';
 
+const main = document.querySelector('#root');
 
-const main = document.querySelector("#root");
-
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   main.appendChild(home());
-})  
+});
 
 // document.querySelector('#root').appendChild(home());
+
+function iconResp() {
+  const addClass = document.querySelector('container');
+
+  if (addClass.className === 'container') {
+    addClass.className += 'ativo';
+  } else {
+    addClass.className = 'container';
+  }
+}
+
+const menu = document.querySelector('icon');
+menu.addEventListener('click', (e) => {
+  e.preventDefault();
+  iconResp();
+});
--- public/pages/login/main.js
+++ public/pages/login/main.js
@@ -1,16 +1,18 @@
-export const login = () =>{
-    const contaner = document.createElement("div");
+export const login = () => {
+  const container = document.createElement('div');
 
-    const template = `
-    <form>
-    <button id = "login-fa-btn">Entrar com o Facebook</button>
-    <button id = "login-go-btn">Entrar com o Google</button>
-    <p>OU</p>
-    <input id = "email" type = "e-mail" placehouder = "Email" >
-    <input id = "full-name" type = "text" placehouder = "Nome completo">
-    <input id = "user-name" type = "text" placehouder = "Nome de usuário">
-    <input id = "password" type = "password" placehouder = "Password">
-    <button id = "login-btn">Cadastrar-se</button>
-    </form>`;
+  const template = `
+  <form>
+  <button id = "login-fa-btn">Entrar com o Facebook</button>
+  <button id = "login-go-btn">Entrar com o Google</button>
+  <p>OU</p>
+  <input id = "email" type = "e-mail" placehouder = "Email" >
+  <input id = "full-name" type = "text" placehouder = "Nome completo">
+  <input id = "user-name" type = "text" placehouder = "Nome de usuário">
+  <input id = "password" type = "password" placehouder = "Password">
+  <button id = "login-btn">Cadastrar-se</button>
+  </form>`;
 
-}
+  container.innerHTML = template;
+  return container;
+};
