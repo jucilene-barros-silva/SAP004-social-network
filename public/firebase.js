a05cb45a02d7c68cd49d33a71eb82f0e66143ecc
Milena Ferraz <milenaferraz@gmail.com>
2020-06-02 15:20:30 -0300

    commit

--- public/firebase.js
+++ public/firebase.js
@@ -1,14 +1,5 @@
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAujpvf2u9ejdy6AsEh93kj-3yQKDmNUQs',
-  authDomain: 'social-network-4801d.firebaseapp.com',
-  databaseURL: 'https://social-network-4801d.firebaseio.com',
-  projectId: 'social-network-4801d',
-  storageBucket: 'social-network-4801d.appspot.com',
-  messagingSenderId: '997757047914',
-  appId: '1:997757047914:web:e88c4fb558eeef8f116613',
-};
 // Initialize Firebase
-export const init = firebase.initializeApp(firebaseConfig);
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
+// export const init = firebase.initializeApp(firebaseConfig);
+// export const auth = firebase.auth();
+// export const firestore = firebase.firestore();
